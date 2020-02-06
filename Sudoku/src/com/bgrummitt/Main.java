7861a442fbb6a8418fd0e0fec381a9d8cbf6bfe3
BenGrumm <ben.grummitt@outlook.com>
2020-02-06 10:38:09 +0000

    Sudoku Solving Project Using Backtracking

---
+++ Sudoku/src/com/bgrummitt/Main.java
@@ -0,0 +1,15 @@
+package com.bgrummitt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Board board = new Board();
+
+	    board.printBoard();
+
+//        SudokuSolver solver = new SudokuSolver(board);
+
+//        solver.solveBoard().printBoard();
+    }
+
+}
