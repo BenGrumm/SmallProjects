5214266e3b00ae6dcd106f5d42cbe16fb05a51c4
BenGrumm <ben.grummitt@outlook.com>
2020-03-17 17:23:39 +0000

    Download Testing

--- GDrive/GDriveTesting/testing.py
+++ GDrive/GDriveTesting/testing.py
@@ -1,13 +1,16 @@
-from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
 gauth = GoogleAuth()
 # Create local webserver and auto handles authentication.
-#gauth.LocalWebserverAuth()
+gauth.LocalWebserverAuth()
+
+from pydrive.drive import GoogleDrive
 
 drive = GoogleDrive(gauth)
 
 # Auto-iterate through all files that matches this query
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
-  print('title: %s, id: %s' % (file1['title'], file1['id']))
+  print('title: %s, id: %s, MimeType: %s' % (file1['title'], file1['id'], file1['mimeType']))
+  if file1['mimeType'] == 'application/vnd.google-apps.document':
+  	file1.GetContentFile(file1['title'], mimetype='text/plain')
