c52d61c8bec059bf3a3bf3bfe32e0b9e52d7c85b
BenGrumm <ben.grummitt@outlook.com>
2020-03-17 17:38:15 +0000

    Folder Traversal Start

--- GDrive/GDriveTesting/testing.py
+++ GDrive/GDriveTesting/testing.py
@@ -12,5 +12,10 @@ drive = GoogleDrive(gauth)
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
   print('title: %s, id: %s, MimeType: %s' % (file1['title'], file1['id'], file1['mimeType']))
+  print('------------------------------------------------------')
+  if file1['mimeType'] == 'application/vnd.google-apps.folder':
+  	subfolder_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % (file1['id'])}).GetList()
+  	for file2 in subfolder_list:
+  		print('title: %s, id: %s, MimeType: %s' % (file2['title'], file2['id'], file2['mimeType']))
   if file1['mimeType'] == 'application/vnd.google-apps.document':
   	file1.GetContentFile(file1['title'], mimetype='text/plain')
