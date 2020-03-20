6058c118b2e16a5dc0fd88b7ab7aa2ef4ddcad88
BenGrumm <ben.grummitt@outlook.com>
2020-03-20 21:42:46 +0000

    Adding basic outline of program operation and making sure folder with all downloaded files gets deleted after use

--- GDrive/GDriveTesting/FileDownloader.py
+++ GDrive/GDriveTesting/FileDownloader.py
@@ -1,5 +1,6 @@
 from pydrive.auth import GoogleAuth
 import shutil
+import sys
 import os
 
 gauth = GoogleAuth()
@@ -17,11 +18,18 @@ drivePath = cwd + driveFolderName
 if not os.path.isdir(drivePath):
 	os.mkdir(drivePath)
 
+def FindInitFolder(folderName):
+	return 'root'
+
+def SearchFolder(stringToFind):
+	return False, ['Could/NotFind', 'CouldNotFind']
+
 def DownloadFIlesInFolder(folder, recursive=True):
 	# Auto-iterate through all files that matches this query
 	file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" %(folder)}).GetList()
 
 	for file in file_list:
+
 		if file['mimeType'] == 'application/vnd.google-apps.folder' and recursive:
 			DownloadFIlesInFolder(file['id'])
 		elif file['mimeType'] != 'application/vnd.google-apps.folder':
@@ -33,5 +41,25 @@ def DownloadFIlesInFolder(folder, recursive=True):
 			# Move the downloaded file into the correct folder
 			shutil.move(cwd + '/' + file['title'], drivePath + '/' + file['title'])
 
+def main(folderToSearch, searchString):
+
+	DownloadFIlesInFolder(FindInitFolder(folderToSearch), False)
+
+	succesDown, filePaths = SearchFolder(searchString)
+
+	if succesDown:
+		print("Found %d occurences of the string %s" %(len(filePaths), searchString))
+
+	if os.path.isdir(drivePath):
+		print("Removing Folder")
+		try:
+			shutil.rmtree(drivePath)
+		except OSError as e:
+			print("Error deleting folder Error: %s" % (e))
+
 if __name__ == '__main__':
-	DownloadFIlesInFolder('root', False)
+
+	if(len(sys.argv) == 2):
+		main(argv[0], argv[1])
+	else:
+		print('Please provide 2 arguments in the form: python FileDownloader.py [Folder Name To Search] [String To Search]')
